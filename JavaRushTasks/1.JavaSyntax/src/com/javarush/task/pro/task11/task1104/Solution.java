f8a826c0651b106423509313582fab60af063318
RomanZvereff <romanzvereff@gmail.com>
2021-04-05 21:26:18 +0300

    21.04.05-lvl12-done

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task11.task1104;
+
+/* 
+Конвертер строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "12.84";
+        //напишите тут ваш код
+        System.out.println((int)Math.round(Double.parseDouble(string)));
+    }
+}
---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task11.task1107;
+
+/* 
+Двигатель — сердце автомобиля
+*/
+
+public class Car {
+    //напишите тут ваш код
+    Engine engine;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+
+    //напишите тут ваш код
+    public class Engine {
+        private boolean isRunning;
+
+        public void start() {
+            isRunning = true;
+        }
+
+        public void stop() {
+            isRunning = false;
+        }
+    }
+}
