1c8e3bdc3f8842d7879a038d202836cda432279d
RomanZvereff <romanzvereff@gmail.com>
2021-04-19 12:35:41 +0300

    lvl21-done-19.04.21

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task11/task1119/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task11.task1119;
+
+/* 
+Четвертая правильная «цепочка наследования»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class House extends Object {
+
+    }
+
+    public class Cat extends Object {
+
+    }
+
+    public class Car extends Object {
+
+    }
+
+    public class Dog extends Object {
+
+    }
+}
---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task11/task1121/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task11.task1121;
+
+/* 
+Чужой код, незнакомый
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet extends Object {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+    public class House extends Object {
+
+    }
+
+    public class Airplane extends Object {
+
+    }
+}
