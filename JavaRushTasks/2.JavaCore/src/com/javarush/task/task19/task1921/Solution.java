5481d220aeb79480222c404ab2f9413b548043f5
RomanZvereff <romanzvereff@gmail.com>
2021-12-21 15:24:20 +0200

    task-2004-done

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -3,9 +3,9 @@ package com.javarush.task.task19.task1921;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 /* 
@@ -13,9 +13,27 @@ import java.util.List;
 */
 
 public class Solution {
-    public static final List<Person> PEOPLE = new ArrayList<Person>();
+    public static final List<Person> PEOPLE = new ArrayList<>();
 
     public static void main(String[] args) {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd M yyyy");
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            int digitIndex = -1;
+            while(reader.ready()) {
+                String input = reader.readLine();
+                for(int i = 0; i < input.length(); i++) {
+                    if(Character.isDigit(input.charAt(i))) {
+                        digitIndex = i;
+                        break;
+                    }
+                }
+                PEOPLE.add(new Person(input.substring(0, digitIndex - 1), formatter.parse(input.substring(digitIndex))));
+            }
+        }catch(IOException | ParseException ex) {
+            System.out.println(ex.getMessage());
+        }
 
     }
 }
